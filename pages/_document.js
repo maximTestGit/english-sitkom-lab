a88efb4267fa996ecebc4babd6e203211f2130be
maximTestGit <32741254+maximTestGit@users.noreply.github.com>
2024-04-09 15:29:44 +0300

    simpler

--- pages/_document.js
+++ pages/_document.js
@@ -2,18 +2,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
-      <Head>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-        />
-        <link rel="stylesheet" href="css/globals.css" />
-      </Head>
+    <Html lang="en">
+      <Head />
       <body>
-        <div className="container">
-          <Main />
-        </div>
+        <Main />
         <NextScript />
       </body>
     </Html>
