b8ac84f088ddc989106f27f048fa39da32def58a
maximTestGit <32741254+maximTestGit@users.noreply.github.com>
2024-04-18 09:10:34 +0300

    add playlist selection

--- components/videoList.js
+++ components/videoList.js
@@ -3,22 +3,38 @@ import VideoRow from "./videoRow";
 import { fetchData } from './helpers/fetchData.js';
 
 const VideoList = ({selectVideo}) => {
+    const default_selected_playList_id = 'PLuMmOfYkVMQ5ZRyYh6tWZm1XTCP-MG1BN';
     const [videos, setVideos] = useState([]);
+    const [selectedPlayListId, setSelectedPlayListId] = useState(default_selected_playList_id);
 
     useEffect(() => {
         const fetchVideos = async () => {
-            let url = `https://us-central1-youtube-project-404109.cloudfunctions.net/function-get-playlst-content`;
+            let url = `https://us-central1-youtube-project-404109.cloudfunctions.net/function-get-playlst-content?playlistId=${selectedPlayListId}`;
             const videos = await fetchData('videos', url, 60*60); // Cache for 1 hour
             setVideos(videos);
         };
         fetchVideos();
-    }, []);
+    }, [selectedPlayListId]);
 
     return (
         <>
-            <table className="table table-hover">
+        <div className="row border border-2 p-1 rounded-3 mb-2">
+            <div className="col-3 text-end">
+        <label className="form-select-lg text-end" htmlFor="playlistSelect">Select Playlist:</label>
+        </div>
+        <div className="col-5">
+        <select className="form-select form-select-lg"
+                    value={selectedPlayListId} 
+                    onChange={(e) => setSelectedPlayListId(e.target.value)}> 
+          <option value={default_selected_playList_id} selected >American Sitcoms</option>
+          <option value="PLuMmOfYkVMQ5P4pAqLFQNMfCxoCRCpvTL">English for Kids</option>
+          <option value="PLuMmOfYkVMQ5BVRFMfLKXmnabpNNIr8yX">Hebrew</option>
+        </select>
+        </div>
+        </div>
+            <table className="table table-hover table-striped">
                 <thead>
-                    <tr>
+                    <tr className="border">
                         <th>YouTube</th>
                         <th>Exercise</th>
                         <th>Description</th>
