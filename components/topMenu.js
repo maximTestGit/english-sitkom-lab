d7023a45d21de5ae18a98232e3e2a703c4bdcec6
maximTestGit <32741254+maximTestGit@users.noreply.github.com>
2024-06-10 21:32:41 +0300

    refactor

--- components/topMenu.js
+++ components/topMenu.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import './styles/topMenu.module.css';
 
-const TopMenu = ({onGoHome}) => {
+const TopMenu = ({ onGoHome }) => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <a className="nav-link nav-link-hover" onClick={()=>onGoHome()} href="" target="_blank" rel="noopener noreferrer">Home</a>
-                    </li>
-                    <li>
-                        <a className="nav-link nav-link-hover" href="https://www.youtube.com/channel/UCWQGpzzCvnv8y8LyH02E2qw" target="_blank" rel="noopener noreferrer">YouTube Channel</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link nav-link-hover" href="mailto:sitcom.english.lab@gmail.com">Contact Developer</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link nav-link-hover" href="#about">About</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <div className='row bg-light'>
+            <a className="nav-link bold-on-hover col-2 col-md-2" onClick={() => onGoHome()} href="" target="" rel="noopener noreferrer">Home</a>
+            <a className="nav-link bold-on-hover col-3 col-md-2" href="https://www.youtube.com/channel/UCWQGpzzCvnv8y8LyH02E2qw" target="_blank" rel="noopener noreferrer">YouTube Channel</a>
+            <a className="nav-link bold-on-hover col-3 col-md-2" href="mailto:sitcom.english.lab@gmail.com">Contact Developer</a>
+        </div>
     );
 };
 
